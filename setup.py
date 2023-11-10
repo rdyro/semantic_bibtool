28c7819eb5b1c4643f68a8bf14842b4fc9be673c
rdyro <rdyro@stanford.edu>
2023-11-10 15:21:38 -0800

    minor fixes for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="semantic_bibtool",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["semantic_bibtool = semantic_bibtool:main"]
